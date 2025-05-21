463e269ce95d1c4ebfede7887247fa7ef929f58c
meghvora16 <voramgh@schaeffler.com>
2025-05-21 19:39:35 +0200

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 import workflow
 
 st.set_page_config(page_title="UV-Vis Analyzer", layout="wide")
-logo_image_path = "Schaeffler_Logo.png"
+logo_image_path = "schaeffler_logo (2).png"
 st.image(logo_image_path, width=150)
 st.title("UV-Vis Spectrum Analyzer")
 
